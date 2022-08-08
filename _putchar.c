6103283d258aa5cfe91660bf126b890f0861bd7f
enoch13 <mwangienoch13@gmail.com>
2022-08-08 10:50:47 +0300

    _putchar file commit

---
+++ _putchar.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static char buffer[1024];
+	static int i;
+
+	if (c == -1)
+	{
+		write(1, &buffer, i);
+		i = 0;
+	}
+	else
+	{
+		buffer[i] = c;
+		i++;
+	}
+	return (1);
+}
